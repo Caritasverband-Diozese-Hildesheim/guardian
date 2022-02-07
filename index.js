350e9c937f2556682acdc694b0f2f5176bb2c1f8
CariSeniorProgrammer <95581231+CariSeniorProgrammer@users.noreply.github.com>
2022-02-07 11:40:50 +0100

    ADD: guardian initial

--- index.js
+++ index.js
@@ -1,28 +1,129 @@
+import session from "express-session";
+import Keycloak from "keycloak-connect";
+
 import { WebSocketServer } from 'ws';
 import express from "express";
 import http from 'http';
 import path from "path";
+import bodyParser from 'body-parser';
 
 const app = express();
 const server = http.createServer(app);
-const wss = new WebSocketServer({ server });
+const wss = new WebSocketServer({ clientTracking: false, noServer: true });
+const connectionMap = new Map();
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
-    ws.send('Hello Client!');
-  });
+const kcConfig = {
+  "client-id": process.env.GW_CLIENTID,
+  "auth-server-url": process.env.GW_AUTHURL,
+  "realm": process.env.GW_REALM,
+  "secret": process.env.GW_CLIENTSECRET,
+  "public-client": false,
+};
+const APIKEY = process.env.GW_APIKEY;
 
-});
+const memoryStore = new session.MemoryStore();
+const keycloak = new Keycloak({ store: memoryStore }, kcConfig);
+
+function msgObject(group, username, adminMsg, msg) {
+  this.group = group,
+    this.username = username,
+    this.adminMsg = adminMsg,
+    this.msg = msg
+}
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-    // #swagger.ignore = true
-    res.render("index", {title: "Guardian Prototype"});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+
+const sessionParser = session({
+  secret: process.env.GW_SESS_KEY,
+  resave: false,
+  saveUninitialized: true,
+  store: memoryStore,
+});
+app.use(sessionParser);
+app.set('trust proxy', true);
+app.use(keycloak.middleware());
+
+app.get("/", keycloak.protect(), (req, res) => {
+  req.session.username = req.kauth.grant.access_token.content.preferred_username;
+  req.session.groups = req.kauth.grant.access_token.content.groups;
+  res.render("index", { title: "Guardian Prototype", hostURL: `\'${process.env.GW_REDIRECT_URL}\'` });
+});
+
+app.post("/sendMessage", (req, res) => {
+
+  if (req.headers['gw_apikey'] === APIKEY) {
+    let counter = 0;
+    const message = new msgObject(req.body.group, req.body.username, req.body.adminMsg, req.body.msg);
+
+    for (const [key, value] of connectionMap.entries()) {
+      console.log(message.username);
+      if (message.group === "all" && (message.msg !== undefined || message.msg !== "")) {
+        value.send(JSON.stringify({ adminMsg: message.adminMsg, msg: message.msg }));
+        counter++;
+      }
+      if (message.group !== undefined && message.username === undefined && (message.msg !== undefined && message.msg !== "")) {
+
+        if (key.groups.includes(message.group)) {
+          value.send(JSON.stringify({ adminMsg: message.adminMsg, msg: message.msg }));
+          counter++;
+        }
+      }
+      if (message.username !== undefined && message.group === undefined && (message.msg !== undefined && message.msg !== "")) {
+        if (key.username === message.username) {
+          value.send(JSON.stringify({ adminMsg: message.adminMsg, msg: message.msg }));
+          counter++;
+        }
+      }
+    }
+    res.status(200).send(`{\"message\":  \"ok\", \"sentCounter\": \"${counter}\"}`);
+  }
+  else {
+    res.status(401).send("Access denied");
+  }
+});
+
+server.on('upgrade', function (request, socket, head) {
+  console.log('Parsing session from request...');
+  sessionParser(request, {}, () => {
+    if (!request.session.username) {
+      socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
+      socket.destroy();
+      return;
+    }
+
+    console.log('Session is parsed!');
+
+    wss.handleUpgrade(request, socket, head, function (ws) {
+      wss.emit('connection', ws, request);
+    });
+  });
+});
+
+wss.on('connection', function connection(ws, request) {
+  const tmpObject = {
+    username: request.session.username,
+    groups: request.session.groups
+  }
+  connectionMap.set(tmpObject, ws)
+  ws.send('Connection to guardian etablished!');
+  
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+    
+  });
+  ws.on('close', function () {
+    connectionMap.delete({ username: request.session.username, groups: request.session.groups });
   });
 
-server.listen(8080, "127.0.0.1");
+});
+
+server.listen(4000, "127.0.0.1", () => {
+  console.log("Server started");
+});
--- public/js/connectWS.js
+++ public/js/connectWS.js
@@ -1,39 +1,53 @@
-const socket = new WebSocket('ws://localhost:8080');
-
-// Connection opened
-socket.addEventListener('open', function (event) {
-    socket.send('Hello Server!');
-});
-
-// Listen for messages
-socket.addEventListener('message', function (event) {
-    console.log(event.data);
-    if (event.data === "Hello Client!") {
-        fetch('http://localhost:5000/ping')
-            .then(response => {
-                console.log(response.status)
-                if (response.status === 200) {
-                    return response.json();
-                }
-                else
-                    alert('Server not reachable')
-                    let data ={};
-                    return data.message="";
-            })
-            .then(data => {
-                if (data.message === "pong") {
-
-                    window.location="http://localhost:5000";
-                }
-            })
-            .catch ((err) => {
-                alert('Server not reachable')
-                    let data ={};
-                    return data.message="";
-            })
+function connectForMessages(URL){
+    var ws = new WebSocket(URL);
+				
+    ws.onopen = function() {
+        ws.onmessage = function (evt) { 
+            if (evt.data === 'Connection to guardian etablished!')
+            {
+                console.log(evt.data);
+            }
+            else 
+            {
+                alert(JSON.parse(evt.data).msg);
+            }
+         };
     }
-});
+}
+function checkForServer(hostURL) {
+    fetch(hostURL + '/ping')
+        .then(response => {
+            console.log(response.status)
+            if (response.status === 200) {
+                return response.json();
+            }
+            else {
+                document.getElementById("message").innerHTML = 'Server not reachable. Please wait, we try again in 10 seconds.';
+                let data = {};
+                return data.message = "";
+            }
+        })
+        .then(data => {
+            if (data.message === "pong") {
+                clearInterval();
+                window.location = hostURL;
+            }
+        })
+        .catch((err) => {
+            document.getElementById("message").innerHTML = 'Server not reachable. Please wait, we try again in 10 seconds.'
 
+            let data = {};
+            return data.message = "";
+        })
+}
+
+function startScanning(hostURL) {
+    connectForMessages('ws://localhost:4000/')
+    checkForServer(hostURL);
+    setInterval(() => {
+        checkForServer(hostURL);
+    }, 10 * 1000);
+};
 
 
 
